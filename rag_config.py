40ec26ef3089d4004fc957bb17818eacd8266380
Shruti Jangale <shruti.j@ergobite.com>
2025-07-29 01:18:16 +0530

    updated model for sentense transformers

--- rag_config.py
+++ rag_config.py
@@ -3,20 +3,15 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv()
-
-
-
 uploadDirectory = "data/uploads"
 indexDirectory = "data/indices"
 textDirectory = "data/raw_texts"
 documentDirectory = "data/documents"
-
+model_dir = r"D:\reference\testing rag\ragChatPDF\all-MiniLM-L6-v2"
 vectorDimension = 384  # or 768 depending on model
 embeddingModel = "all-MiniLM-L6-v2"  # adjust as needed
-groqApiKey = os.getenv("GROQ_API_KEY")
 OCR_APPLICATION_FILE_PATH = r"tesseract_ocr\\tesseract.exe"
 TESSERACT_MODEL = "eng_all"
-
 
 def ensureDirs():
     os.makedirs(uploadDirectory, exist_ok=True)
